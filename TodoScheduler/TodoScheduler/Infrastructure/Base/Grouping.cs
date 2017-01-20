1589343f420d9f4d04376d2bd2ad5882d71f4ea9
ihor fastnakht <fastnakht.ihor@gmail.com>
2017-01-20 22:02:59 +0200

    Create grouping<K,T> generic class
    created SelectableObject

---
+++ TodoScheduler/TodoScheduler/Infrastructure/Base/Grouping.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TodoScheduler.Infrastructure.Base
+{
+    public class Grouping<TKey, TItem> : ObservableCollection<TItem>
+    {
+        public TKey Key { get; private set; }
+
+        public Grouping(TKey _key, IEnumerable<TItem> _items)
+        {
+            Key = _key;
+
+            foreach (var i in _items)
+                this.Items.Add(i);
+        }
+    }
+}
