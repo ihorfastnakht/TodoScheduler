e6e86e4f3c2756d236191ecfb0c148d3654f074f
ihor fastnakht <fastnakht.ihor@gmail.com>
2017-01-20 23:35:28 +0200

    created menu Items for page model

---
+++ TodoScheduler/TodoScheduler/PageModels/MenuGroup.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoScheduler.PageModels
+{
+    public class MenuGroup
+    {
+        public int Id { get; set; }
+        public string Icon { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ TodoScheduler/TodoScheduler/PageModels/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoScheduler.PageModels
+{
+    public class MenuItem
+    {
+        public MenuGroup MenuGroup { get; set; }
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public Type ViewModelType;
+    }
+}
