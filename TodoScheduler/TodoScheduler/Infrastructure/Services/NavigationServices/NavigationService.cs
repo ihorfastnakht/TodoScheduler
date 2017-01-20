a9599503a4f333c37b59bf9d320b76ec184f2d27
ihor fastnakht <fastnakht.ihor@gmail.com>
2017-01-20 22:59:15 +0200

    created navigation service and partially implemented INavigationService

---
+++ TodoScheduler/TodoScheduler/Infrastructure/Services/NavigationServices/NavigationService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoScheduler.Infrastructure.Enums;
+using Xamarin.Forms;
+
+namespace TodoScheduler.Infrastructure.Services.NavigationServices
+{
+    public class NavigationService : INavigationService
+    {
+        #region members
+
+        private INavigation _navigation;
+
+        #endregion
+
+        #region INavigationService implementation
+
+        public async Task CloseAsync(NavType navType = NavType.Stack, bool animation = false)
+        {
+            switch (navType)
+            {
+                case NavType.Stack:
+                    await _navigation.PopAsync(animation);
+                    break;
+                case NavType.Modal:
+                    await _navigation.PopModalAsync(animation);
+                    break;
+                default:
+                    throw new Exception("Navigation is not valid");
+            }
+        }
+
+        public async Task NavigateAsync(Type viewModelType, Dictionary<string, object> parameters = null, 
+            NavType navType = NavType.Stack, bool animation = false)
+        {
+            switch (navType)
+            {
+                case NavType.Stack:
+                    throw new NotImplementedException();
+                    break;
+                case NavType.Modal:
+                    throw new NotImplementedException();
+                    break;
+                case NavType.ReplaceRoot:
+                    throw new NotImplementedException();
+                    break;
+                default:
+                    throw new NotImplementedException();
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
