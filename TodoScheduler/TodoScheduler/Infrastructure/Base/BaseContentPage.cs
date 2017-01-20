b9734b214c46620cd24ae6af7a01bc876acd4fce
ihor fastnakht <fastnakht.ihor@gmail.com>
2017-01-20 22:47:44 +0200

    created BaseContentPage

---
+++ TodoScheduler/TodoScheduler/Infrastructure/Base/BaseContentPage.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace TodoScheduler.Infrastructure.Base
+{
+    public class BaseContentPage : ContentPage
+    {
+        public BaseContentPage()
+        {
+            if (Device.OS == TargetPlatform.iOS)
+                this.Padding = new Thickness(0,20,0,0);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var viewModel = this.BindingContext as ViewModelBase;
+
+            if (viewModel != null)
+                viewModel.Appearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+    }
+}
