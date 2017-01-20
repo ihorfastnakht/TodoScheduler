aa431a0ac53ae2afb990832065ec222e01009637
ihor fastnakht <fastnakht.ihor@gmail.com>
2017-01-20 21:51:54 +0200

    Created base ObservableObject

---
+++ TodoScheduler/TodoScheduler/Infrastructure/Base/ObservableObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TodoScheduler.Infrastructure.Base
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region protected
+
+        protected bool SetProperty<T>(ref T backedField, T value, [CallerMemberName] string propertyName = "",
+            string[] dependendProperty = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backedField, value)) return false;
+
+            if (dependendProperty != null)
+            {
+                foreach (var property in dependendProperty)
+                    OnPropertyChanged(property);
+            }
+
+            backedField = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion
+    }
+}
