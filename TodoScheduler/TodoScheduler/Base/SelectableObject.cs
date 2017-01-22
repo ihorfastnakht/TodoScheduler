d1528e3a3fec1df752f23daa4fd2bb1c94e8b089
ihor fastnakht <fastnakht.ihor@gmail.com>
2017-01-22 19:03:01 +0200

    Created Grouping<K,T> and Selectable<T> generic objects

---
+++ TodoScheduler/TodoScheduler/Base/SelectableObject.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoScheduler.Base
+{
+    public class SelectableObject<T> : ObservableObject
+    {
+        T _item;
+        public T Item {
+            get { return _item; }
+            set { SetProperty(ref _item, value); }
+        }
+
+        bool _isSelected = false;
+        public bool IsSelected {
+            get { return _isSelected; }
+            set { SetProperty(ref _isSelected, value); }
+        }   
+    }
+}
