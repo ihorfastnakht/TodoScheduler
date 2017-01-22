9a05870052ff3194157240c29a21ac86a9391c5b
ihor fastnakht <fastnakht.ihor@gmail.com>
2017-01-22 18:51:33 +0200

    Created bases abstract ObservableObject and ViewModelBase

---
+++ TodoScheduler/TodoScheduler/Base/ObservableObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TodoScheduler.Base
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region protected
+
+        protected bool SetProperty<T>(ref T backedField, T value, [CallerMemberName] string propertyName = "",
+            string[] dependendProperties = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backedField, value)) return false;
+
+            if (dependendProperties != null)
+            {
+                foreach (var property in dependendProperties)
+                    OnPropertyChanged(property);
+            }
+
+            backedField = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion
+    }
+}
---
+++ TodoScheduler/TodoScheduler/Base/ViewModelBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace TodoScheduler.Base
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+
+        #region fields & properties
+
+        string _icon;
+        public string Icon
+        {
+            get { return _icon; }
+            set { SetProperty(ref _icon, value); }
+        }
+
+        string _header;
+        public string Header
+        {
+            get { return _header; }
+            set { SetProperty(ref _header, value); }
+        }
+
+        #endregion
+
+        #region virtual
+
+        public virtual void Init(Dictionary<string, object> parameters = null) { }
+        public virtual void Appearing() { }
+        public virtual void Disappearing() { }
+
+        #endregion
+    }
+}
