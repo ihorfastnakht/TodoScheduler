0a7ef31634b7ebdf4c9e1134fd7f05410a588074
Ігор Фастнахт <fastnakht.ihor@gmail.com>
2017-01-25 17:05:10 +0200

    update CreateTodoPage

--- TodoScheduler/TodoScheduler/Pages/CreateTodoPage.xaml.cs
+++ TodoScheduler/TodoScheduler/Pages/CreateTodoPage.xaml.cs
@@ -7,6 +7,23 @@ namespace TodoScheduler.Pages
         public CreateTodoPage()
         {
             InitializeComponent();
+
+            //TODO: scroll to end
+
+            @switch.Toggled += (s, e) =>
+            {
+                if (e.Value)
+                {
+                    reminderContainer.HeightRequest = 130;
+                    holder.IsVisible = true;
+                }
+                else
+                {
+                    reminderContainer.HeightRequest = 32;
+                    holder.IsVisible = false;
+                }
+                //ForceLayout();
+            };
         }
     }
 }
